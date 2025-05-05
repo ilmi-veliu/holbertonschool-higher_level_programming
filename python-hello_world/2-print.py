bde6ad9ccfedea549cc11de5d0d27c8a10d495a8
ilmi-veliu <iveliu011@gmail.com>
2025-05-05 10:53:40 +0200

    betty

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-print('\"Programming is like building a multilingual puzzle')
+
+# Script qui affiche une phrase exacte
+# Utilise la fonction print pour afficher le message
+print('"Programming is like building a multilingual puzzle')
