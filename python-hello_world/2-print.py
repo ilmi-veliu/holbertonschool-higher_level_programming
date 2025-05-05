411c3d79ae954cc2996b891649216a5800f04a34
ilmi-veliu <iveliu011@gmail.com>
2025-05-05 09:25:06 +0200

    rajout de l anti slash

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 # This script prints a quote to the console using the print() function.
 
-print('"Programming is like building a multilingual puzzle')
+print('\"Programming is like building a multilingual puzzle')
