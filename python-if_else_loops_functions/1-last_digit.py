a2db05df8788cb69c78082079354dd645485d0c3
ilmi-veliu <iveliu011@gmail.com>
2025-05-06 11:44:46 +0200

    test

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,10 +4,10 @@ import random
 number = random.randint(-10000, 10000)
 
 if number % 10 > 5:
-    print('the string and is greater than 5')
+    print('and is greater than 5')
 
 elif number % 10 == 0:
-    print('the string and is 0')
+    print('and is 0')
 
 else:
-    print('the string and is less than 6 and not 0')
+    print('and is less than 6 and not 0')
