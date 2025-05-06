2566eeba10dad4200e6660e1abffd2caef59b49b
ilmi-veliu <iveliu011@gmail.com>
2025-05-06 11:43:17 +0200

    last-random

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+if number % 10 > 5:
+    print('the string and is greater than 5')
+
+elif number % 10 == 0:
+    print('the string and is 0')
+
+else:
+    print('the string and is less than 6 and not 0')
