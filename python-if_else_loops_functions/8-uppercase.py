fc9a4205bdfc34069293c32cd3c99e7d228d30f7
ilmi-veliu <iveliu011@gmail.com>
2025-05-07 13:07:02 +0200

    sup des 'a'

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 def uppercase(str):
     result = ""
     for i in str:
-        if 'a' <= i <= 'z':
+        if ord(i) >= 97 and ord(i) <= 122:
             result += chr(ord(i) - 32)
         else:
             result += i
