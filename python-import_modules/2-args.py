0642f4eba6a33a98a5fae9180104441ffba2fae3
ilmi-veliu <iveliu011@gmail.com>
2025-05-08 21:03:52 +0200

    syntaxe corec

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 import sys
+
 if __name__ == "__main__":
-    len(sys.argv) - 1
     argument_count = len(sys.argv) - 1
+
     if argument_count == 0:
-        print("{} arguments.".format(argument_count))
-    
+        print("0 arguments.")
     elif argument_count == 1:
+        print("1 argument:")
         print("1: {}".format(sys.argv[1]))
-    
     else:
-        print(argument_count,' arguments :')
-        for i in range(1,len(sys.argv)):
-            print('{}: {}'.format(i ,sys.argv[i]))
+        print("{} arguments:".format(argument_count))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
