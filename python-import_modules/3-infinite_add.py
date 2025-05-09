9d7ad8fa44be2fa1350389ef129454e467f6e4a0
ilmi-veliu <iveliu011@gmail.com>
2025-05-09 08:40:54 +0200

    codestyle

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,9 +2,9 @@
 import sys
 
 if __name__ == "__main__":
-    adition = 0
+    addition = 0
 
-    for i in range(1,len(sys.argv)):
-        adition += int(sys.argv[i])
-    
-    print(adition)
+    for i in range(1, len(sys.argv)):
+        addition += int(sys.argv[i])
+
+    print(addition)
