114870299aacec0ace92acebf093d0271dac8bee
ilmi-veliu <iveliu011@gmail.com>
2025-05-12 19:41:55 +0200

    codestyle

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     my_list_2 = my_list[:]
-    
+
     if idx < 0:
         return my_list_2
-    
-    elif idx >= len(my_list):
+    if idx >= len(my_list):
         return my_list_2
-    
-    else:
-         my_list_2[idx] = element
-         return my_list_2
+
+    my_list_2[idx] = element
+    return my_list_2
