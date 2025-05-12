f86bf069ad3b74a88d1d24ebb773a718fbb5f475
ilmi-veliu <iveliu011@gmail.com>
2025-05-12 19:34:37 +0200

    4

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    my_list_2 = my_list[:]
+    
+    if idx < 0:
+        return my_list_2
+    
+    elif idx >= len(my_list):
+        return my_list_2
+    
+    else:
+         my_list_2[3] = element
