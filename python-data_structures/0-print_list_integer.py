38cae1af6234131e100d464395460f95adc9e0c5
ilmi-veliu <iveliu011@gmail.com>
2025-05-12 22:49:37 +0200

    fsdfs

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
 def print_list_integer(my_list=[]):
-
-    for integer in my_list:
-        print("{:d}".format(integer))
+    for i in my_list:
+        print("{:d}".format(i))
