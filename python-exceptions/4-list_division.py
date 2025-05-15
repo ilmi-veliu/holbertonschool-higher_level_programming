a4f9ecec4a2dae698d9d3f16f59e23147b0995f7
ilmi-veliu <iveliu011@gmail.com>
2025-05-15 09:52:19 +0200

    add append au exept

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -5,19 +5,17 @@ def list_division(my_list_1, my_list_2, list_length):
 
     for i in range(list_length):
         try:
-            nvx_list.append( my_list_1[i] / my_list_2[i])
+            nvx_list.append(my_list_1[i] / my_list_2[i])
         except ZeroDivisionError:
-              nvx_list.append(0)
-              print("division by 0")
-        
+            print("division by 0")
+            nvx_list.append(0)
         except TypeError:
             print("wrong type")
-
+            nvx_list.append(0)
         except IndexError:
             print("out of range")
-        
+            nvx_list.append(0)
         finally:
             pass
 
-    
     return nvx_list
