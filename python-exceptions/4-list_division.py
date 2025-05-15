cf082f1b040d7e03357d8d96303fad6c9d21d668
ilmi-veliu <iveliu011@gmail.com>
2025-05-15 09:50:04 +0200

    ajout des spe

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -8,5 +8,16 @@ def list_division(my_list_1, my_list_2, list_length):
             nvx_list.append( my_list_1[i] / my_list_2[i])
         except ZeroDivisionError:
               nvx_list.append(0)
+              print("division by 0")
+        
+        except TypeError:
+            print("wrong type")
+
+        except IndexError:
+            print("out of range")
+        
+        finally:
+            pass
+
     
     return nvx_list
