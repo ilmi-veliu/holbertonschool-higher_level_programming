f70a3a28b5be8997ada893c7fe11cc5a2c8e2340
ilmi-veliu <iveliu011@gmail.com>
2025-05-17 15:08:13 +0200

    pyd

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -10,17 +10,20 @@ def matrix_divided(matrix, div):
     """
     if not isinstance(matrix, list) or not all(
             isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
 
     if not all(isinstance(num, (int, float))
                for row in matrix for num in row):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
 
     row_lengths = [len(row) for row in matrix]
     if row_lengths.count(row_lengths[0]) != len(row_lengths):
         raise TypeError("Each row of the matrix must have the same size")
 
-    if not isinstance(div, (int, float)) or div != div or abs(div) == float('inf'):
+    if not isinstance(div, (int, float)) or div != div or \
+            abs(div) == float('inf'):
         raise TypeError("div must be a number")
 
     if div == 0:
