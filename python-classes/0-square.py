6488d6bfbdaed34386d0452d134ea5464ed02dee
ilmi-veliu <iveliu011@gmail.com>
2025-05-19 11:26:52 +0200

    0

---
+++ python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    Représente un carré vide.
+
+    Cette classe ne fait rien pour l'instant, elle sert de base pour créer
+    un carré avec des attributs ou méthodes plus tard.
+    """
+    pass
+
+
+test_square = Square()
