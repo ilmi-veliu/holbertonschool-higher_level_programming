d208ea80ded9a45f59715736499fcb47161ffb97
ilmi-veliu <iveliu011@gmail.com>
2025-05-26 15:20:24 +0200

    test

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
 Module that defines the MyList class.
+This module defines a class that inherits from list
+and includes a method to print the list in sorted order.
 """
 
 
@@ -15,3 +17,11 @@ class MyList(list):
         """
         panda = sorted(self)
         print(panda)
+
+
+if __name__ == "__main__":
+    """
+    Example usage of the MyList class.
+    """
+    my_list = MyList([3, 1, 4, 2])
+    my_list.print_sorted()
