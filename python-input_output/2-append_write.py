6c5293cc5ab8936a723451117f571140a13ef05e
ilmi-veliu <iveliu011@gmail.com>
2025-06-02 23:05:24 +0200

    pep 8

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Append a string at the end of a file and return the number of characters written"""
+"""Append a string at the end of a file and return the number of characters written."""
+
 
 def append_write(filename="", text=""):
-    """Append text to a file and return number of characters written"""
+    """Append text to a file and return number of characters written."""
     with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
