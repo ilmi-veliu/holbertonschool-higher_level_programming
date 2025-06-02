d00f25888b063541e4bec35e55099cc7ea94a8ea
ilmi-veliu <iveliu011@gmail.com>
2025-06-02 13:49:54 +0200

    1

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""a function that reads a text file (UTF8) and prints it to stdout"""
+
+
 def read_file(filename=""):
-    """Reads a text file (UTF-8) and prints it to stdout."""
-    with open(filename, "r", encoding="utf-8") as p:
-        print(p.read())
+    """a function that reads a text file (UTF8) and prints it to stdout"""
+    with open(filename) as file:
+        content = file.read()
+        print(content)
