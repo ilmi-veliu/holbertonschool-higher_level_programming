f54686be897f67f8f9f42a9c4034665021d8263e
ilmi-veliu <iveliu011@gmail.com>
2025-06-03 14:19:39 +0200

    ds

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-"""Class that defines a student."""
+"""Module defining the Student class."""
+
 
 class Student:
+    """Represents a student."""
+
     def __init__(self, first_name, last_name, age):
+        """Initialize attributes."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """Return dictionary representation."""
         return self.__dict__
