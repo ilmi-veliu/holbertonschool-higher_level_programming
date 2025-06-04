36279ad7a3f3a28ca3c0dc39eb6d6b08bcb48409
ilmi-veliu <iveliu011@gmail.com>
2025-06-04 10:16:49 +0200

    pep

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """Module defining the Student class."""
-
+ 
 
 class Student:
     """Represents a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a student with first name, last name, and age."""
+        """Initialize a student."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Return dictionary representation, filtered if attrs is a list of strings."""
+        """Return a dictionary representation, filtered by attrs if provided."""
         if type(attrs) is not list:
             return self.__dict__
 
