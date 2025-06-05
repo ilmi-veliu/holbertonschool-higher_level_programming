0863389ec03350f5377293764e0f7dc0a0018a5d
ilmi-veliu <iveliu011@gmail.com>
2025-06-05 16:04:10 +0200

    pep

--- python-serialization/00_basic_serialization.py
+++ python-serialization/00_basic_serialization.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python3
+"""Basic serialization module using JSON format."""
+
 import json
 
+
 def serialize_and_save_to_file(data, filename):
-    """Save dict to JSON file."""
-    with open(filename, "w") as f:
-        json.dump(data, f)
+    """
+    Serialize a Python dictionary and save it to a JSON file.
+
+    Args:
+        data (dict): The dictionary to serialize.
+        filename (str): The name of the file to save the JSON data to.
+
+    Returns:
+        None
+    """
+    with open(filename, "w") as file:
+        json.dump(data, file)
+
 
 def load_and_deserialize(filename):
-    """Load dict from JSON file."""
-    with open(filename, "r") as p:
-        return json.load(p)
+    """
+    Load JSON data from a file and deserialize it to a Python dictionary.
+
+    Args:
+        filename (str): The name of the JSON file to read.
+
+    Returns:
+        dict: The deserialized dictionary.
+    """
+    with open(filename, "r") as file:
+        return json.load(file)
