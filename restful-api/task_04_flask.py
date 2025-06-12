7f4937c86a971f75657b4f4815f03ebe396ac97c
ilmi-veliu <iveliu011@gmail.com>
2025-06-12 21:25:20 +0200

    4

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -2,12 +2,10 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# Route racine
 @app.route("/")
 def home():
     return "Welcome to the Flask API!"
 
-# Route JSON de test
 @app.route("/data")
 def get_data():
     sample_data = {
@@ -17,28 +15,12 @@ def get_data():
     }
     return jsonify(sample_data)
 
-# Route de statut
 @app.route("/status")
 def status():
     return "OK"
 
-# Base de données simulée : dictionnaire des utilisateurs
-users = {
-    "john": {
-        "username": "john",
-        "name": "John",
-        "age": 30,
-        "city": "New York"
-    },
-    "jane": {
-        "username": "jane",
-        "name": "Jane",
-        "age": 28,
-        "city": "Los Angeles"
-    }
-}
+users = {}
 
-# Route dynamique pour récupérer un utilisateur
 @app.route("/users/<username>")
 def get_user(username):
     if username in users:
@@ -46,7 +28,6 @@ def get_user(username):
     else:
         return jsonify({"error": "User not found"}), 404
 
-# Route POST pour ajouter un utilisateur
 @app.route("/add_user", methods=["POST"])
 def add_user():
     data = request.get_json()
