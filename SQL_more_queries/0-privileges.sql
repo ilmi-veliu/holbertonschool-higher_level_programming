2804dcd7e05ae7954ea75a986d2bfd97dbb434bc
ilmi-veliu <iveliu011@gmail.com>
2025-06-25 14:03:19 +0200

    com

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,6 +1,7 @@
--- List all privileges of user_0d_1 and user_0d_2
+-- Create user_0d_1 only if it does not already exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
+-- Create user_0d_2 only if it does not already exist
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
