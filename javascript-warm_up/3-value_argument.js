ee5d989665c2aa5d9ecc9e7ee9f6d3069d2e02d8
ilmi-veliu <iveliu011@gmail.com>
2025-07-11 10:48:53 +0200

    3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
-
-if (args == 0)
-    {
-        console.log('No argument');
-    }
-
-    else
-        {
-            console.log(args);
-        }
+if (process.argv[2]) {
+    console.log(process.argv[2]);
+  } else {
+    console.log('No argument');
+  }
+  
