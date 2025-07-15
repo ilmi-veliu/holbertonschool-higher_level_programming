70ca11305623b62609a294d16c22bfd6e217111c
ilmi-veliu <iveliu011@gmail.com>
2025-07-15 14:59:15 +0200

    5

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+document.querySelector('#update_header').addEventListener('click', function () {
+    const header = document.querySelector('header');
+    header.textContent = 'New Header!!!';
+  });
+  
